e4b5d49ed8fe33cccce083b227e5fed6765f6ad9
Oleksandr Pavlii <pavliy.av@gmail.com>
2025-07-31 16:20:15 +0200

    Add user model methods for fetching and saving data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,7 @@
 import { User } from './models/User';
 
-const user = new User({ name: "John", age: 20 });
 
-user.set({ age: 55 });
-console.log(user.get('age'));
+const user = new User({ id: 1 });
 
-user.on('change', () => { console.log('change'); })
-user.on('change', () => { console.log('change 2'); })
-
-user.trigger('change')
-
-console.log(user);
+user.set({ name: 'Bob' });
+// user.save();
